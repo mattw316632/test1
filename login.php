c11e7616f2cb63ec4a2f434128e3b533bab1eac3
mattw316632 <matthew.watson15@bathspa.ac.uk>
2018-04-12 06:42:34 +0100

    first

--- login.php
+++ login.php
@@ -2,13 +2,15 @@
 <?php 
 
     $host = "mysql:dbname=db1;unix_socket=/cloudsql/mattw316632:europe-west1:test01";
-    $database = "test01";
+    $database = "db1";
     $username = "user";
     $password ="";
     
-    $conn = new mysqli(null, $username, '',
-'db1', null, '/cloudsql/mattw316632:europe-west1:test01');
-    //$conn = new mysqli($host, $username, $passtword);
+    //$conn = new mysqli(null, $username, '',
+//'db1', null, '/cloudsql/mattw316632:europe-west1:test01');
+    $conn = new mysqli('/cloudsql/mattw316632:europe-west1:test01', $username, $passtword, 'db1');
+
+    //$conn = new mysqli($host, $username, $passtword, $database);
     $json = file_get_contents('php://input');  
  
     $obj = json_decode($json,true);
