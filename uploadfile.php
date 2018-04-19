e9fa1a08c47b6a0b94f80a820087c08f3b099677
mattw316632 <matthew.watson15@bathspa.ac.uk>
2018-04-19 18:39:19 +0100

    second22

--- uploadfile.php
+++ uploadfile.php
@@ -1,19 +1,55 @@
-<?php  
-use google\appengine\api\cloud_storage\CloudStorageTools;  
-$bucket = ‘ '; // your bucket name  
-$root_path = 'gs://' . $bucket . '/';  
-$_url = '';  
-if(isset($_POST['submit']))  
-{  
-    if(isset($_FILES['userfile']))  
-    {  
-        $name = $_FILES['userfile']['name'];  
-        $file_size =$_FILES['userfile']['size'];  
-        $file_tmp =$_FILES['userfile']['tmp_name'];  
-        $original = $root_path .$name;  
-        move_uploaded_file($file_tmp, $original);  
-        $_url=CloudStorageTools::getImageServingUrl($original);  
-    }  
-}  
-  
-?>  
+<?php 
+
+    $host = "mysql:dbname=db1;unix_socket=/cloudsql/mattw316632:europe-west1:test01";
+    $database = "db1";
+    $username = "user";
+    $password ="";
+    
+    $conn = new mysqli(null, $username, '',
+'db1', null, '/cloudsql/mattw316632-201000:europe-west1:test01');
+    
+
+    //$conn = new mysqli($host, $username, $passtword, $database);
+    $json = file_get_contents('php://input');  
+ 
+    $obj = json_decode($json,true);
+    $userId= $obj["username"];
+    $name = $obj["name"];
+    $longitude = $obj["longitude"];
+    $latitude = $obj["latitude"];
+    $data = $obj["data"];
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $sql = "select * from image where user_id='$userId'";
+    $signup = "INSERT INTO image(id, user_id ,name, longitude, latitude, bump, data) VALUES(NULL,'$userID', '$name', '$longitude','$latitude', 0, '$data')";
+    $result = $conn->query($sql);
+    
+    if($obj["name"] != null){
+        if($result->num_rows>=0){
+            $add = $conn->query($signup);
+            if($add == true){
+                $outputObj->success = true;
+                $outputObj->image = $name;
+                $outputObj->message = "image uploaded";
+            
+                echo json_encode($outputObj);
+            }
+            
+        }
+        else{
+            $outputObj->success = false;
+            echo json_encode($outputObj); 
+        }
+    }
+    else{
+        $outputObj->success = false;
+        $outputObj->message = "Please enter a name";
+        
+        echo json_encode($outputObj); 
+    }
+ 
+    $conn->close();
+    
